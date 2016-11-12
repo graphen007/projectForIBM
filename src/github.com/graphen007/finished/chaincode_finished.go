8d75ed620eca92c61b4569baaa8fd1a3bd80839f
mikkel.victor.petersen@ibm.com <bonde_325@hotmail.com>
2016-11-12 17:30:56 +0100

    working commit ass this

--- src/github.com/graphen007/finished/chaincode_finished.go
+++ src/github.com/graphen007/finished/chaincode_finished.go
@@ -20,7 +20,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledgers/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
